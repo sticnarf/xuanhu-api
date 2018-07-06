cfdc6e0a728e65167575c73d9406db71961b159d
Yilin Chen <sticnarf@gmail.com>
2018-07-06 14:56:24 +0800

    Add user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,35 @@
 class UsersController < ApplicationController
   before_action :require_login, only: [:current]
+  before_action :require_self, only: [:update]
 
   def create
-    user = User.new(
-      email: params[:email].downcase,
-      name: params[:name],
-      password: params[:password],
-      is_teacher: false
-    )
+    user_params = params.permit(:email, :name, :password)
+    user = User.new(user_params)
     if user.save
       login user
       render json: user, except: [:password_digest]
     else
-      render json: user.errors
+      render json: user.errors, status: 422
     end
   end
 
   def update
-    
+    user_params = params.permit(:name, :avatar_url)
+    if current_user.update_attributes(user_params)
+      render json: current_user, except: [:password_digest]
+    else
+      render json: current_user.errors, status: 422
+    end
   end
 
   def current
     render json: current_user, except: [:password_digest]
+  end
+
+  private
+  def require_self
+    if current_user&.id&.to_s != params[:id]
+      render json: { success: false }, status: 403
+    end
   end
 end
